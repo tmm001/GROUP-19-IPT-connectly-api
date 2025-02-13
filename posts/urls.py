f8d554861856fa0804ddd0e8e8b418f37787e13a
Therese Marie Guinto <lr.tmguinto@mmdc.mcl.edu.ph>
2025-02-13 16:08:57 +0000

    Refactor URL routing to use class-based views for users, posts, and comments

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from . import views
+from .views import UserListCreate, PostListCreate, CommentListCreate
 
 urlpatterns = [
-    path('users/', views.get_users, name='get_users'),
-    path('users/create/', views.create_user, name='create_user'),
-    path('users/update/<int:id>/', views.update_user, name='update_user'),
-    path('users/delete/<int:id>/', views.delete_user, name='delete_user'),
+    path('users/', UserListCreate.as_view(), name='user-list-create'),
+    path('posts/', PostListCreate.as_view(), name='post-list-create'),
+    path('comments/', CommentListCreate.as_view(), name='comment-list-create'),
 ]
