6541527c7a1555d780aa6fe7bc20092562e40eeb
Therese Marie Guinto <lr.tmguinto@mmdc.mcl.edu.ph>
2025-02-13 18:35:17 +0000

    Refactor URL routing in the project to improve formatting and readability

--- g19_connectly_project/urls.py
+++ g19_connectly_project/urls.py
@@ -18,14 +18,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
-     TokenObtainPairView,
-     TokenRefreshView,
- )
-
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 urlpatterns = [
-     path('admin/', admin.site.urls),
-     path('api-auth/', include('rest_framework.urls')),
-     path('posts/', include('posts.urls')),
-     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'), # Get token
-     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # Refresh token
- ]
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('posts/', include('posts.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'), # Get token
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # Refresh token
+]
